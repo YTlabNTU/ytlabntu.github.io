540b6e5462921576c96b77a4827ff2e606a1c9fe
YTlabNTU <ytlabpub@gmail.com>
2024-05-16 03:17:51 +0800

    Create team.js

---
+++ assets/js/team.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.team-member').forEach(item => {
+  item.addEventListener('mouseover', event => {
+    event.target.querySelector('img').classList.remove('grayscale-effect');
+    event.target.querySelector('img').classList.add('colorful-effect');
+  });
+  
+  item.addEventListener('mouseout', event => {
+    event.target.querySelector('img').classList.remove('colorful-effect');
+    event.target.querySelector('img').classList.add('grayscale-effect');
+  });
+});
