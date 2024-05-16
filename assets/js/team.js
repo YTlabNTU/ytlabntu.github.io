2521b10c2519f178e7f297bf7c21beaffcff74d4
YTlabNTU <ytlabpub@gmail.com>
2024-05-16 15:04:34 +0800

    Update team.js

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,11 +1,18 @@
-console.log("Hello, world!")
-
-document.querySelectorAll('.media.mb-5').forEach(item => {
-    item.addEventListener('mouseover', event => {
-        event.currentTarget.querySelector('img').style.filter = 'none';
-    });
-  
-    item.addEventListener('mouseout', event => {
-        event.currentTarget.querySelector('img').style.filter = 'grayscale(100%)';
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Hello, world!");
+    
+    document.querySelectorAll('.media.mb-5').forEach(item => {
+        item.addEventListener('mouseover', event => {
+            event.currentTarget.querySelector('img').style.filter = 'none';
+        });
+      
+        item.addEventListener('mouseout', event => {
+            event.currentTarget.querySelector('img').style.filter = 'grayscale(100%)';
+        });
     });
 });
+
+
+
+
+
