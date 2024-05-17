a386212c908c14debe3fbc7ccb73800fe9bf7c7b
YTlabNTU <ytlabpub@gmail.com>
2024-05-17 15:03:59 +0800

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,17 +1,32 @@
-// gallery.js
+let slideIndex = 0;
+showSlides();
 
-let slideIndex = 1;
-showSlides(slideIndex);
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlidesManual(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlidesManual(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlidesManual(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
