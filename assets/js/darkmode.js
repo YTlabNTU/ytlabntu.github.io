808e97538410da15d05d67d66c8d4b38307a6d89
YTlabNTU <ytlabpub@gmail.com>
2024-05-21 13:18:17 +0800

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,19 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.createElement('button');
-    toggleButton.id = 'darkModeToggle';
-    toggleButton.innerHTML = '&#9790;'; // Moon icon
-    toggleButton.style.position = 'fixed';
-    toggleButton.style.top = '10px';
-    toggleButton.style.right = '10px';
-    toggleButton.style.zIndex = '1000';
-    toggleButton.style.border = 'none'; // Remove button border
-    toggleButton.style.background = 'none'; // Remove button background
-    toggleButton.style.cursor = 'pointer'; // Change cursor to pointer on hover
-    toggleButton.style.fontSize = '24px'; // Increase icon size
-    toggleButton.style.color = 'black'; // Set icon color
-
-    document.body.insertBefore(toggleButton, document.body.firstChild);
-
+    const toggleButton = document.getElementById('darkModeToggle');
+    
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
